35180824da761a3bc47e4af3dd93e64f32452d6a
seercle <axel.vivenot@outlook.fr>
2025-07-21 21:42:21 +0200

    now passing multiple versions of nixpkgs

--- flake.nix
+++ flake.nix
@@ -5,45 +5,56 @@
     nixpkgs-24-05.url = "github:nixos/nixpkgs?ref=nixos-24.05";
     nixpkgs-25-05.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
+    home-manager-24-05 = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs-24-05";
     };
+    home-manager-25-05 = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs-25-05";
+    };
   };
-  outputs = inputs@{self, nixpkgs-24-05, nixpkgs-25-05, nixpkgs-unstable, home-manager,...}:
+  outputs = inputs@{self, nixpkgs-24-05, nixpkgs-25-05, nixpkgs-unstable, home-manager-24-05, home-manager-25-05, ...}:
 
   let
+    getPkgs = nixpkgs: nixpkgs.legacyPackages.${system};
+
     profile = "homelab"; #profile to select, must be contained in the profiles directory
     hostname = "homelab";
     system = "x86_64-linux";
     users = ["axel" "william"]; #users to select, must be contained in the users directory of the profile directory
+
+    nixpkgs = nixpkgs-24-05;
+    home-manager = home-manager-24-05;
+    pkgs = {
+      pkgs24-05 = getPkgs nixpkgs-24-05;
+      pkgs25-05 = getPkgs nixpkgs-25-05;
+      pkgsUnstable = getPkgs nixpkgs-unstable;
+    }
   in {
     homeConfigurations = builtins.listToAttrs (builtins.map(user: {
       name = user;
       value = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs-24-05.legacyPackages.${system};
+        pkgs = getPkgs nixpkgs;
         modules = [
           ./home.nix
           ./profiles/${profile}/home.nix
           ./profiles/${profile}/users/${user}/home.nix
         ];
         extraSpecialArgs = {
-          inherit user;
+          inherit user pkgs;
         };
       };
     }) users);
 
-    nixosConfigurations.${hostname} = nixpkgs-24-05.lib.nixosSystem {
+    nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
       system = system;
       modules = [
         ./configuration.nix
         ./profiles/${profile}/configuration.nix
       ] ++ builtins.map (username: ./profiles/${profile}/users/${username}/configuration.nix) users;
       specialArgs = {
-        inherit users hostname;
-        pkgs24-05 = nixpkgs-24-05.legacyPackages.${system};
-        pkgs25-05 = nixpkgs-25-05.legacyPackages.${system};
-        pkgsUnstable = nixpkgs-unstable.legacyPackages.${system};
+        inherit users hostname pkgs;
       };
     };
   };

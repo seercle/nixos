99b2ca8fcba68c04ef43be965b0c5bbd1a1cd14a
seercle <axel.vivenot@outlook.fr>
2025-07-21 21:50:41 +0200

    to fix

--- flake.nix
+++ flake.nix
@@ -17,44 +17,43 @@
   outputs = inputs@{self, nixpkgs-24-05, nixpkgs-25-05, nixpkgs-unstable, home-manager-24-05, home-manager-25-05, ...}:
 
   let
-    getPkgs = nixpkgs: nixpkgs.legacyPackages.${system};
+    #getPkgs = some_nixpkgs: some_nixpkgs.legacyPackages.${system};
 
     profile = "homelab"; #profile to select, must be contained in the profiles directory
     hostname = "homelab";
     system = "x86_64-linux";
     users = ["axel" "william"]; #users to select, must be contained in the users directory of the profile directory
-
-    nixpkgs = nixpkgs-24-05;
-    home-manager = home-manager-24-05;
-    pkgs = {
-      pkgs24-05 = getPkgs nixpkgs-24-05;
-      pkgs25-05 = getPkgs nixpkgs-25-05;
-      pkgsUnstable = getPkgs nixpkgs-unstable;
-    }
+    nixpkgs-version = nixpkgs-24-05;
+    home-manager-version = home-manager-24-05;
+    allPkgs = {
+      pkgs24-05 = nixpkgs-24-05.legacyPackages.${system};
+      pkgs25-05 = nixpkgs-25-05.legacyPackages.${system};
+      pkgsUnstable = nixpkgs-unstable.legacyPackages.${system};
+    };
   in {
     homeConfigurations = builtins.listToAttrs (builtins.map(user: {
       name = user;
-      value = home-manager.lib.homeManagerConfiguration {
-        pkgs = getPkgs nixpkgs;
+      value = home-manager-version.lib.homeManagerConfiguration {
+        pkgs = nixpkgs-version.legacyPackages.${system};
         modules = [
           ./home.nix
           ./profiles/${profile}/home.nix
           ./profiles/${profile}/users/${user}/home.nix
         ];
         extraSpecialArgs = {
-          inherit user pkgs;
+          inherit user allPkgs;
         };
       };
     }) users);
 
-    nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.${hostname} = nixpkgs-version.lib.nixosSystem {
       system = system;
       modules = [
         ./configuration.nix
         ./profiles/${profile}/configuration.nix
       ] ++ builtins.map (username: ./profiles/${profile}/users/${username}/configuration.nix) users;
       specialArgs = {
-        inherit users hostname pkgs;
+        inherit users hostname allPkgs;
       };
     };
   };

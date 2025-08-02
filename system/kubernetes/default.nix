f56d3ec9d90a26aeb53f7cb1a6838e51861fad6b
seercle <notseercle@gmail.com>
2025-08-02 15:27:21 +0200

    sealed

--- system/kubernetes/default.nix
+++ system/kubernetes/default.nix
@@ -13,6 +13,7 @@ in {
     ./minio
     ./postgresql
     ./redis
+    ./sealed-secrets
   ];
   sops.secrets = {
     K3S_TOKEN = {};
@@ -21,7 +22,7 @@ in {
     allowedTCPPorts = [
       6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
       2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-      # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+      2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
     ];
     allowedUDPPorts = [
       8472 # k3s, flannel: required if using multi-node for inter-node networking

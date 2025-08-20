1fbee787757678436eb24c49acfab47bc5534bf5
seercle <notseercle@gmail.com>
2025-08-20 17:28:10 +0200

    changed ssh key

--- profiles/homelab/users/axel/configuration.nix
+++ profiles/homelab/users/axel/configuration.nix
@@ -7,7 +7,7 @@
     homeMode = "770";
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBImNr8lQYL9vG3TfamWzfuUC6xX/kF2bRdhE3h8U98G axel.vivenot@outlook.fr"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBImNr8lQYL9vG3TfamWzfuUC6xX/kF2bRdhE3h8U98G accel@arch"
     ];
   };
 }
--- profiles/homelab/users/william/configuration.nix
+++ profiles/homelab/users/william/configuration.nix
@@ -6,7 +6,7 @@
     extraGroups = ["networkmanager" "wheel"];
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBImNr8lQYL9vG3TfamWzfuUC6xX/kF2bRdhE3h8U98G axel.vivenot@outlook.fr"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBImNr8lQYL9vG3TfamWzfuUC6xX/kF2bRdhE3h8U98G accel@arch"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINDffWe7kGAcZIQSZ9tcKwNyWEe9lRXYPMBmEvzlH85I william.driot@telecom-paris.fr"
     ];
   };

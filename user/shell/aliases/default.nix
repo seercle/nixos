e257e241d778be6b1b7cce9d8effaf5312a65aea
seercle <notseercle@gmail.com>
2025-09-25 14:36:39 +0200

    nfu

--- user/shell/aliases/default.nix
+++ user/shell/aliases/default.nix
@@ -4,6 +4,7 @@
     hms = "home-manager switch --flake /etc/nixos/profiles/${profile}";
     nrs = "sudo nixos-rebuild switch --flake /etc/nixos/profiles/${profile}";
     ngc = "nixos-generate-config --dir /etc/nixos/profiles/${profile}";
+    nfu = "nix flake update --flake /etc/nixos/profiles/${profile}";
     ls = "eza --icons -l -T -L=1 --group-directories-first";
     grep = "rg";
     htop = "btm";

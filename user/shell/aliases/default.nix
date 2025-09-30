ccd9b7f4a90846a16b31b00036437344e930b1d8
seercle <notseercle@gmail.com>
2025-09-30 13:10:26 +0200

    git aliases

--- user/shell/aliases/default.nix
+++ user/shell/aliases/default.nix
@@ -5,6 +5,14 @@
     nrs = "sudo nixos-rebuild switch --flake /etc/nixos/profiles/${profile}";
     ngc = "nixos-generate-config --dir /etc/nixos/profiles/${profile}";
     nfu = "nix flake update --flake /etc/nixos/profiles/${profile}";
+
+    ga = "git add .";
+    gc = "git commit -am";
+    gp = "git push";
+    gl = "git pull";
+    gs = "git status";
+    gd = "git diff";
+
     ls = "eza --icons -l -T -L=1 --group-directories-first";
     grep = "rg";
     htop = "btm";
@@ -12,6 +20,7 @@
     neofetch = "fastfetch";
     gitfetch = "onefetch";
     cat = "bat";
+
     s = "sudo";
     k = "kubectl";
     zed = "zeditor";
